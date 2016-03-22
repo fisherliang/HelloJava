d365bbaebe6334843c2df1b661b0b76cb5bd3684
Fisher <fisherliang.tw@gmail.com>
2016-03-22 13:50:20 +0800

    Demo for File Manipulation

--- HelloJava/src/greetings/HelloJava.java
+++ HelloJava/src/greetings/HelloJava.java
@@ -1,5 +1,10 @@
 package greetings;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import printing.ColorCartridge;
 import printing.Printer;
 
@@ -13,8 +18,33 @@ public class HelloJava {
 		printer.loadPaper(5);
 		printer.print(3);
 		printer.outPutPage(2);
-
 		
+		Path path = Paths.get("D:\\workspace\\SubTest\\newfile.txt");
+		
+		//Creating a file
+		try
+		{
+			Files.createFile(path);
+			
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		
+//		//Deleting a File
+//		try
+//		{
+//			Files.deleteIfExists(path);
+//		}catch (IOException e){
+//			e.printStackTrace();
+//		}
+		
+		//Moving a File
+		try
+		{
+			Files.move(path, Paths.get("D:\\workspace\\SubTest\\movedfile.txt"));
+		}catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 		
 }
