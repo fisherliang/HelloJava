4b001f945ffc6de4e14b5633a92f6b294d9b2a22
Fisher <fisherliang.tw@gmail.com>
2016-03-22 16:04:39 +0800

    Demo for Custom Annotation

--- HelloJava/src/greetings/HelloJava.java
+++ HelloJava/src/greetings/HelloJava.java
@@ -1,18 +1,29 @@
 package greetings;
 
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import printing.ColorCartridge;
 import printing.Printer;
+import printing.PrintingDevice;
 
 public class HelloJava {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
 	{
 		
 		Printer<ColorCartridge> printer = new Printer<ColorCartridge>(true, "My Printer", ColorCartridge.blue);
 		
-		printer.loadPaper(5);
-		printer.outPutPage(10);
+		printer.loadPaper(10);
+		
+		PrintingDevice annotation = printer.getClass().getAnnotation(PrintingDevice.class);
+		
+		Method printMethod = printer.getClass().getMethod(annotation.defaultPrintMethod(), int.class);
+		
+		printMethod.invoke(printer, annotation.defaultNumberOfCopies());
+		
+		printer.outPutPage(5);
 		
 	}
 		
