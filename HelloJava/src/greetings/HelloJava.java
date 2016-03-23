b6662e27b62a19fcb639d7029b4ce0e68bd8f632
Fisher <fisherliang.tw@gmail.com>
2016-03-23 10:01:44 +0800

    Demo for Executors

--- HelloJava/src/greetings/HelloJava.java
+++ HelloJava/src/greetings/HelloJava.java
@@ -2,6 +2,8 @@ package greetings;
 
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import printing.ContinuousPrinter;
 
@@ -12,8 +14,21 @@ public class HelloJava {
 	{
 		ContinuousPrinter cp = new ContinuousPrinter();
 		
-		Thread thread = new Thread(cp);
-		thread.start();
+//		Thread thread = new Thread(cp);
+//		thread.start();
+		
+		//ExecutorService executor = Executors.newFixedThreadPool(10);
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		
+		executor.submit(cp);
+		executor.submit(cp);
+		executor.submit(cp);
+		executor.submit(cp);
+		executor.submit(cp);
+		executor.submit(cp);
+		
+		executor.shutdown();
+		
 		
 		for(int i=0;i<100;i++)
 			System.out.println("Main Thread : " + i);
